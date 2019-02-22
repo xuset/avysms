96c987e60e8d356abd9fcd5796b7c871f5099beb
Austin Middleton <xuset1@gmail.com>
2019-02-21 19:56:38 -0700

    Add lambda logging and headers

--- lambda_entrypoint.py
+++ lambda_entrypoint.py
@@ -4,14 +4,19 @@ import sys
 from download_caic_forecast import retrieve_forecast
 from forecast_to_readable import convert_forecast_to_human_readable_text
 
-
 def lambda_handler(event, context):
+  print('event=lambda_invoked, lambda_event={}, lambda_context={}'.format(event, context), file=sys.stderr)
   sangres_zone_id = 9
   forecast = retrieve_forecast(sangres_zone_id)
-  return {
+  result = {
     "statusCode": 200,
+    "headers": {
+      "Content-Type": "text/plain"
+    },
     "body": convert_forecast_to_human_readable_text(forecast)
   }
+  print('event=lambda_return, result={}'.format(result), file=sys.stderr)
+  return result
 
 if __name__ == "__main__":
   json.dump(lambda_handler(None, None), sys.stdout)
